35a31dfbda756471e02bbdb178de0f46e9ce6903
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-06 15:42:41 +0200

    Update makeVolcano.R

--- makeVolcano.R
+++ makeVolcano.R
@@ -1,19 +1,19 @@
 makeVolcano <- function(df, contrast) {
   
   suppressWarnings(
-    df %>%
-      filter(contrast == contrast) %>%
-      mutate(regulation = case_when(
-        padj < 0.05 & logFC > 1 ~ "Upregulated",
-        padj < 0.05 & logFC < -1 ~ "Downregulated",
+    deg_res %>%
+      filter(Contrast == contrast) %>%
+      mutate(Regulation = case_when(
+        Padj < 0.05 & LogFC > 1 ~ "Upregulated",
+        Padj < 0.05 & LogFC < -1 ~ "Downregulated",
         TRUE ~ "None"
       )) %>%
-      mutate(labels = ifelse(gene_id %in% top_degs, gene_id, NA_character_)) %>%
-      ggplot(aes(x = logFC, y = -log10(padj), col = regulation, label = labels)) +
-      geom_point(size = 2, alpha = 0.8) +
+      mutate(Labels = ifelse(Gene_id %in% top_degs, Gene_id, NA_character_)) %>%
+      ggplot(aes(x = LogFC, y = -log10(Padj), col = Regulation, label = Labels)) +
+      geom_point(size = 2, alpha = 0.5) +
       ggrepel::geom_label_repel(
         size = 3, label.size = 0.5, fontface = 2, show.legend = FALSE,
-        box.padding = 0.5, point.padding = 0.5, nudge_x = 0.5,
+        box.padding = 0.5, point.padding = 0.5, nudge_x = 0.5, nudge_y = 0.5,
         arrow = arrow(length = unit(x = 0.25, units = "cm")), max.overlaps = Inf
       ) +
       geom_vline(xintercept = c(-1, 1), col = "black", lty = "dashed") +
@@ -22,18 +22,25 @@ makeVolcano <- function(df, contrast) {
         breaks = c("Upregulated", "Downregulated", "None"),
         values = c("firebrick1", "royalblue1", "gray50")
       ) +
-      scale_x_continuous(breaks = seq.int(
-        from = ceiling(min(all_res$logFC)),
-        to = ceiling(max(all_res$logFC)),
-        by = 1
-      )) +
+      scale_x_continuous(
+        breaks = seq.int(
+          from = floor(min(deg_res$LogFC)),
+          to = ceiling(max(deg_res$LogFC)),
+          by = 1
+        ),
+        limits = c(
+          floor(min(deg_res$LogFC)),
+          ceiling(max(deg_res$LogFC))
+        )
+      ) +
       labs(
         x = expression(log[2]~fold~change),
         y = expression(-log[10]~adjusted~p.value),
         color = NULL,
-        title = contrast %>% str_to_title(),
-        subtitle = "Top DEGs"
+        title = paste0("Comparison: ", str_to_title(contrast)),
+        subtitle = "Top 10 up- and down-regulated DEGs"
       ) +
+      guides(colour = guide_legend(override.aes = list(size = 5))) +
       theme(
         text = element_text(size = 14, color = "black"),
         plot.title = element_text(face = "bold", hjust = 0.5, size = 16),
@@ -41,10 +48,10 @@ makeVolcano <- function(df, contrast) {
         legend.position = "top",
         legend.direction = "horizontal",
         panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(color = "gray85"),
+        panel.grid.major = element_line(color = "gray90"),
         panel.background = element_rect(fill = "white"),
         panel.border = element_rect(fill = NA, color = "black", size = 1),
-        legend.background = element_blank()
+        legend.key = element_rect(fill = "white")
       )
   )
   
